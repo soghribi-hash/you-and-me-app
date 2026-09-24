74d1ebe5a1358fada77cdb5b227050fed85aaaaf
soghribi-hash <so.ghribi@gmail.com>
2026-09-24 19:18:03 +0200

    Youandme16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "you-and-me-v14";
+const CACHE = "you-and-me-v16";
 const FILES = ["./", "./index.html", "./style.css", "./app.js", "./firebase-config.js", "./spotify-config.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
