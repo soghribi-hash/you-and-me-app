786a4e8c99134b817a3eebd67cc42f5f470a54eb
soghribi-hash <so.ghribi@gmail.com>
2026-09-24 08:35:47 +0200

    you-and-me-v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "you-and-me-v9";
+const CACHE = "you-and-me-v11";
 const FILES = ["./", "./index.html", "./style.css", "./app.js", "./firebase-config.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
